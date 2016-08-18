7c7af84cb2f0b860a8875a68a3f8c184f4a985be
Longor <lars195@yahoo.de>
2016-08-19 01:39:12 +0200

    Eraser Tool: Now has selective-erase mode!

--- src/java/de/longor/talecraft/items/EraserItem.java
+++ src/java/de/longor/talecraft/items/EraserItem.java
@@ -1,6 +1,6 @@
 package de.longor.talecraft.items;
 
-import de.longor.talecraft.util.WorldHelper;
+import de.longor.talecraft.util.MutableBlockPos;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
@@ -19,23 +19,53 @@ public class EraserItem extends TCItem {
 
 	@Override
 	public EnumActionResult onItemUse(ItemStack stack, EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
-		onItemRightClick(stack, world, player, hand);
-
-		return EnumActionResult.SUCCESS;
+		if(world.isRemote)
+			return EnumActionResult.PASS;
+		
+		// Cant prevent double calls? Welp, time for ugly hacks!
+		if(isDoubleCall(stack, world.getTotalWorldTime())) {
+			return EnumActionResult.PASS;
+		}
+		
+		if(player.isSneaking()) {
+			IBlockState mask = world.getBlockState(pos);
+			
+			if(applyEraser(world, player, mask))
+				return EnumActionResult.SUCCESS;
+			else
+				return EnumActionResult.SUCCESS;
+		} else {
+			return onItemRightClick(stack, world, player, hand).getType();
+		}
 	}
 
 	@Override
 	public ActionResult<ItemStack> onItemRightClick(ItemStack stack, World world, EntityPlayer player, EnumHand hand) {
 		if(world.isRemote)
 			return ActionResult.newResult(EnumActionResult.PASS, stack);
-
-		IBlockState state = Blocks.AIR.getDefaultState();
-
+		
+		if(isDoubleCall(stack, world.getTotalWorldTime())) {
+			return ActionResult.newResult(EnumActionResult.PASS, stack);
+		}
+		
+		if(!applyEraser(world, player, null)) {
+			return ActionResult.newResult(EnumActionResult.SUCCESS, stack);
+		}
+		
+		return ActionResult.newResult(EnumActionResult.SUCCESS, stack);
+	}
+	
+	public static final boolean applyEraser(World world, EntityPlayer player, IBlockState mask) {
 		// Note: the bounds are already sorted
 		int[] bounds = WandItem.getBoundsFromPlayerOrNull(player);
 
 		if(bounds == null) {
-			return ActionResult.newResult(EnumActionResult.PASS, stack);
+			// Woopsy!
+			if(player instanceof EntityPlayerMP) {
+				String msg = TextFormatting.RED + "ERROR: NO BLOCKS SELECTED.";
+				((EntityPlayerMP)player).addChatMessage(new TextComponentString(msg));
+			}
+			return false;
 		}
 
 		final int maxvolume = 64*64*64;
@@ -47,12 +77,39 @@ public class EraserItem extends TCItem {
 				String msg = TextFormatting.RED + "ERROR: TOO MANY BLOCKS TO ERASE -> " + volume + " >= " + maxvolume;
 				((EntityPlayerMP)player).addChatMessage(new TextComponentString(msg));
 			}
-			return ActionResult.newResult(EnumActionResult.FAIL, stack);
+			return false;
 		}
-
-		WorldHelper.fill(world, bounds[0], bounds[1], bounds[2], bounds[3], bounds[4], bounds[5], state);
-
-		return ActionResult.newResult(EnumActionResult.SUCCESS, stack);
+		
+		applyEraser(world, mask, bounds);
+		return true;
+	}
+	
+	public static final void applyEraser(World world, IBlockState mask, int[] bounds) {
+		int ix=bounds[0],iy=bounds[1],iz=bounds[2],ax=bounds[3],ay=bounds[4],az=bounds[5];
+		
+		IBlockState air = Blocks.AIR.getDefaultState();
+		MutableBlockPos pos = new MutableBlockPos(0, 0, 0);
+		
+		if(mask == null) {
+			for(int y = iy; y <= ay; y++) {
+				for(int z = iz; z <= az; z++) {
+					for(int x = ix; x <= ax; x++) {
+						pos.set(x, y, z);
+						world.setBlockState(new BlockPos(pos), air);
+					}
+				}
+			}
+		} else {
+			for(int y = iy; y <= ay; y++) {
+				for(int z = iz; z <= az; z++) {
+					for(int x = ix; x <= ax; x++) {
+						pos.set(x, y, z);
+						if(world.getBlockState(pos).equals(mask))
+							world.setBlockState(new BlockPos(pos), air);
+					}
+				}
+			}
+		}
 	}
 
 }
--- src/java/de/longor/talecraft/items/TCItem.java
+++ src/java/de/longor/talecraft/items/TCItem.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
@@ -77,6 +78,27 @@ public class TCItem extends Item {
 	public boolean onBlockStartBreak(ItemStack stack, BlockPos pos, EntityPlayer player) {
 		player.worldObj.notifyBlockUpdate(pos, player.worldObj.getBlockState(pos), player.worldObj.getBlockState(pos), 0);
 		return true;
+	}
+	
+	public static final boolean isDoubleCall(ItemStack stack, long currentWorldTime) {
+		NBTTagCompound compound = stack.getTagCompound();
+		if(compound!=null){
+			// Double Call Prevention Hack
+			long timeNow = currentWorldTime;
+			long timePre = compound.getLong("DCPH");
+			
+			if(timeNow == timePre) {
+				return true;
+			} else {
+				compound.setLong("DCPH", timeNow);
+				return false;
+			}
+		} else {
+			NBTTagCompound nbt = new NBTTagCompound();
+			nbt.setLong("DCPH", currentWorldTime);
+			stack.setTagCompound(nbt);
+		}
+		return false;
 	}
 
 }
