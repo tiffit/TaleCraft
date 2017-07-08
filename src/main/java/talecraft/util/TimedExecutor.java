fe5d1ea6829e462fe2371db9aebcc616cc9b3c04
Longor <lalongok@gmail.com>
2017-07-08 22:03:11 +0200

    Replaced TimedExecutor with SheduledExecutorService

--- src/main/java/talecraft/util/TimedExecutor.java
+++ src/main/java/talecraft/util/TimedExecutor.java
@@ -2,60 +2,18 @@ package talecraft.util;
 
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
-public class TimedExecutor implements Runnable {
-	private class TimedRunnable {
-		long time;
-		Runnable run;
-		public TimedRunnable(Runnable call, long flashpoint) {
-			time = flashpoint;
-			run = call;
-		}
-	}
-
-	private final ConcurrentLinkedDeque<TimedRunnable> runners;
-	private final Thread thread;
-
+public class TimedExecutor {
+	private final ScheduledExecutorService executor;
 	public TimedExecutor() {
-		runners = new ConcurrentLinkedDeque<TimedExecutor.TimedRunnable>();
-
-		thread = new Thread(this);
-		thread.setName("timed-executor");
-		thread.setDaemon(true);
-		thread.start();
+		executor = Executors.newScheduledThreadPool(1);
 	}
-
+	
 	public void executeLater(Runnable runnable, int time) {
-		long current = System.currentTimeMillis();
-		long flashpoint = current + time;
-		runners.add(new TimedRunnable(runnable, flashpoint));
+		executor.schedule(runnable, time, TimeUnit.MILLISECONDS);
 	}
-
-	@Override
-	public void run() {
-		ArrayList<TimedRunnable> tempL = new ArrayList<TimedRunnable>();
-
-		while(!Thread.interrupted()) {
-			long current = System.currentTimeMillis();
-
-			// check which runnables can be run
-			for(TimedRunnable r : runners) {
-				if(current > r.time) {
-					tempL.add(r);
-				}
-			}
-			while(tempL.size() > 0) {
-				TimedRunnable r = tempL.get(0);
-				runners.remove(r);
-				r.run.run();
-			}
-
-			try {
-				Thread.sleep(50);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
+	
 }
