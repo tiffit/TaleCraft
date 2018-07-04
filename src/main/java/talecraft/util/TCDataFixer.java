ee43bfc38e9972c7618ece348449b83280daeb60
ErrorCraftLP <Nils.tePoel@gmx.de>
2018-07-04 15:51:20 +0200

    Added support for converting tile entity IDs from pre-v0.3.0 TaleCraft worlds

--- src/main/java/talecraft/util/TCDataFixer.java
+++ src/main/java/talecraft/util/TCDataFixer.java
@@ -35,6 +35,30 @@ public class TCDataFixer implements IFixableData {
         OLD_TO_NEW_ID_MAP.put("minecraft:tc_summonblock", Reference.MOD_ID + ":summonblock");
         OLD_TO_NEW_ID_MAP.put("minecraft:tc_musicblock", Reference.MOD_ID + ":musicblock");
         OLD_TO_NEW_ID_MAP.put("minecraft:tc_camerablock", Reference.MOD_ID + ":camerablock");
+        
+    	OLD_TO_NEW_ID_MAP.put("tc_lockeddoorblock", Reference.MOD_ID + ":lockeddoorblock");
+        OLD_TO_NEW_ID_MAP.put("tc_killblock", Reference.MOD_ID + ":killblock");
+        OLD_TO_NEW_ID_MAP.put("tc_clockblock", Reference.MOD_ID + ":clockblock");
+        OLD_TO_NEW_ID_MAP.put("tc_redstone_trigger", Reference.MOD_ID + ":redstone_trigger");
+        OLD_TO_NEW_ID_MAP.put("tc_redstone_activator", Reference.MOD_ID + ":redstone_activator");
+        OLD_TO_NEW_ID_MAP.put("tc_relayblock", Reference.MOD_ID + ":relayblock");
+        OLD_TO_NEW_ID_MAP.put("tc_scriptblock", Reference.MOD_ID + ":scriptblock");
+        OLD_TO_NEW_ID_MAP.put("tc_updatedetectorblock", Reference.MOD_ID + ":updatedetectorblock");
+        OLD_TO_NEW_ID_MAP.put("tc_storageblock", Reference.MOD_ID + ":storageblock");
+        OLD_TO_NEW_ID_MAP.put("tc_emitterblock", Reference.MOD_ID + ":emitterblock");
+        OLD_TO_NEW_ID_MAP.put("tc_imagehologramblock", Reference.MOD_ID + ":imagehologramblock");
+        OLD_TO_NEW_ID_MAP.put("tc_collisiontriggerblock", Reference.MOD_ID + ":collisiontriggerblock");
+        OLD_TO_NEW_ID_MAP.put("tc_lightblock", Reference.MOD_ID + ":lightblock");
+        OLD_TO_NEW_ID_MAP.put("tc_hiddenblock", Reference.MOD_ID + ":hiddenblock");
+        OLD_TO_NEW_ID_MAP.put("tc_messageblock", Reference.MOD_ID + ":messageblock");
+        OLD_TO_NEW_ID_MAP.put("tc_inverterblock", Reference.MOD_ID + ":inverterblock");
+        OLD_TO_NEW_ID_MAP.put("tc_memoryblock", Reference.MOD_ID + ":memoryblock");
+        OLD_TO_NEW_ID_MAP.put("tc_triggerfilterblock", Reference.MOD_ID + ":triggerfilterblock");
+        OLD_TO_NEW_ID_MAP.put("tc_delayblock", Reference.MOD_ID + ":delayblock");
+        OLD_TO_NEW_ID_MAP.put(":tc_urlblock", Reference.MOD_ID + ":urlblock");
+        OLD_TO_NEW_ID_MAP.put("tc_summonblock", Reference.MOD_ID + ":summonblock");
+        OLD_TO_NEW_ID_MAP.put("tc_musicblock", Reference.MOD_ID + ":musicblock");
+        OLD_TO_NEW_ID_MAP.put("tc_camerablock", Reference.MOD_ID + ":camerablock");
     }
 
     public int getFixVersion() {
